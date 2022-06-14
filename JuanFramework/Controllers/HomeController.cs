9f3a7a34489bad3a1880282a888b417bc9cbe28c
Sekine Nezerova <7b4gz6k@code.edu.az>
2022-06-14 13:53:14 +0400

    commit task

---
+++ JuanFramework/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using JuanFramework.DAL;
+using JuanFramework.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JuanFramework.Controllers
+{
+    public class HomeController : Controller
+    {
+
+
+        private readonly AppDbContext db;
+        public HomeController(AppDbContext _db)
+        {
+            db = _db;
+        }
+
+        public IActionResult Index()
+        {
+           
+            return View();
+        }
+
+       
+    }
+}
---
+++ JuanFramework/Models/Category.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JuanFramework.Models
+{
+    public class Category :BaseEntity
+    {
+        public int Name { get; set; }
+        public List<Product> Products { get; set; }
+    }
+}
